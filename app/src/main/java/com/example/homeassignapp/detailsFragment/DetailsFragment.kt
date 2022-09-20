05f768f6b630dac1e67a9f7386c4aba9a1d91049
rtomaz <rodrigo.tomaz.contractor@hilton.com>
2022-09-20 16:54:10 -0700

    Include view-binding & initialize fragments boiler plate.

--- app/src/main/java/com/example/homeassignapp/detailsFragment/DetailsFragment.kt
+++ app/src/main/java/com/example/homeassignapp/detailsFragment/DetailsFragment.kt
@@ -1,23 +1,31 @@
 package com.example.homeassignapp.detailsFragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.homeassignapp.R
+import androidx.fragment.app.Fragment
+import com.example.homeassignapp.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
