d205643ae54609c31629ba04713744abfd5c991b
rtomaz <rodrigo.tomaz.contractor@hilton.com>
2022-10-28 15:03:13 -0700

    Initial implementation of the recycler view layout.

---
+++ app/src/main/java/com/example/homeassignapp/adapters/RecViewAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.homeassignapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.homeassignapp.R
+import com.example.homeassignapp.retrofit.PhotoItem
+
+class RecViewAdapter(private val list: List<PhotoItem>) : RecyclerView.Adapter<RecViewAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView
+        val textView: TextView
+
+        init {
+            imageView = itemView.findViewById(R.id.rvCardImg)
+            textView = itemView.findViewById(R.id.rvCardTitle)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = list[position]
+
+        holder.textView.text = item.title
+        holder.imageView.load(item.getPhotoURL())
+    }
+
+    override fun getItemCount() = list.size
+}
