85a1382b6b0a9fc0e9a7f07f660bf9999c27c703
rtomaz <rodrigo.tomaz.contractor@hilton.com>
2022-10-28 15:59:58 -0700

    Call recyclerView adapter on RecViewFragment.kt & minor layout adjustments.

--- app/src/main/java/com/example/homeassignapp/fragments/RecViewFragment.kt
+++ app/src/main/java/com/example/homeassignapp/fragments/RecViewFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.homeassignapp.adapters.RecViewAdapter
 import com.example.homeassignapp.databinding.FragmentRecViewBinding
+import com.example.homeassignapp.viewmodels.SharedViewModel
 
 class RecViewFragment : Fragment() {
 
     private var _binding: FragmentRecViewBinding? = null
     private val binding get() = _binding!!
+
+    private val svm: SharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,13 @@ class RecViewFragment : Fragment() {
 
         _binding = FragmentRecViewBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        svm.photoList.observe(viewLifecycleOwner) { list ->
+            list?.let {
+                val adapter = RecViewAdapter(it)
+                binding.recyclerView.adapter = adapter
+            }
+        }
 
         return view
     }
